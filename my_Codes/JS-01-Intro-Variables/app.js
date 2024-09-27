75f6df36d175b741d4e0d203136460139b470eb2
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-27 23:19:28 +0200

    second commit

---
+++ my_Codes/JS-01-Intro-Variables/app.js
@@ -0,0 +1,20 @@
+console.log("external js'den selamlar");
+
+// bu bir yorum satiridir
+
+console.log("burasi console alani");
+
+console.error("bu bir hata mesajidir");
+
+console.warn("bu bir uyari mesajidir");
+
+//console.clear(); // console temizlemek icin kullanilir
+
+//alert("alert mesaji");
+
+//! prompt kullanimi:
+
+console.log(prompt("lütfen adinizi giriniz"));
+
+name1 = prompt("lütfen adinizi giriniz");
+console.log(name1);
