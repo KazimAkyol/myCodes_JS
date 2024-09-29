d0a388e686260a701bb9b8dc78065b9d9f587ab9
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-29 20:14:07 +0200

    17.commit

---
+++ my_Codes/JS-03-Conditionals/1-if-else if-else.js
@@ -0,0 +1,30 @@
+//! ************ IF - ELSE ********** //
+
+// if (sart) {
+//     sart durumu dogru ise bu blok calistirilir.
+// }else{
+//     sart durumu dogru degilse bu blok calistirilir.
+// }
+
+let sicaklik = 30;
+
+if (sicaklik > 50) {
+  console.log("yikamaya basla");
+} else {
+  console.log("suyu isitmaya devam et");
+}
+
+//! prompt, kodu kullanicidan bilgi almayi saglar.
+//! Kullanicidan alinan her bilgi string olarak gelir.
+
+//* Girilen bir sayinin tek mi, cift mi oldugunu yazan program:
+
+// let sayi = Number(prompt("Bir sayi giriniz"));
+
+// let kalan = sayi % 2;
+
+// if (kalan === 0) {
+//   console.log("Sayi cifttir");
+// } else {
+//   console.log("Sayi tektir");
+// }
