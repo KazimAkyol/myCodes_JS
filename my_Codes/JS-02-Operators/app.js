5cfd533ad51eae6978ac051a71f557a3e865c19b
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-29 16:34:13 +0200

    10.commit

--- my_Codes/JS-02-Operators/app.js
+++ my_Codes/JS-02-Operators/app.js
@@ -46,3 +46,43 @@ console.log("benim adim soyadim " + isim + " " + soyisim); //benim adim soyadim 
 //! BACKTICK (template literals) ``
 
 console.log(`benim adim soyadim ${isim} ${soyisim} `); //benim adim soyadim mehmet kara
+
+//? bazi fonksiyonlar
+
+//* Math.floor(); her zaman en yakin alt tam sayiya yuvarlar.
+//* Math.ceil(); her zaman en yakin üst tam sayiya yuvarlar.
+//* Math.trunc(); sayinin tam kismini alir.
+//* Math.round(); en yakin tam sayiya yuvarlar.
+
+//* Math.random(); 0 ve 1 arasinda rastgele sayi üretir.
+
+//* toFixed(x); virgülden sonra x adet basamak birakir.
+
+const ab = 1.3;
+const cd = 1.8;
+console.log(Math.floor(ab)); //1
+console.log(Math.floor(cd)); //1
+
+console.log(Math.ceil(ab)); //2
+console.log(Math.ceil(cd)); //2
+
+console.log(Math.trunc(ab)); //1
+console.log(Math.trunc(cd)); //1
+
+console.log(Math.round(ab)); //1
+console.log(Math.round(cd)); //2
+
+console.log((3.145674893939).toFixed(3)); //3.146
+
+console.log(Math.random()); // 0-1 arasinda rastgele bir sayi üretir.
+
+//document.write(Math.trunc(ab));
+
+//* RANDOM: rastgele, rastgele sayi üretmek icin kullanilir.
+//* 0-1 arasinda bir sayi üretir.
+console.log(Math.random());
+console.log(Math.ceil(Math.random() * 20)); // 1-20 arasinda rastgele bir sayi üretir.
+
+//* ARTTIRMA, AZALTMA
+
+let a = 5;
