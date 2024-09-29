afd685a96b2e925faa7303c267475cdd9d2fb494
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-29 20:40:15 +0200

    18.commit

--- my_Codes/JS-03-Conditionals/1-if-else if-else.js
+++ my_Codes/JS-03-Conditionals/1-if-else if-else.js
@@ -28,3 +28,52 @@ if (sicaklik > 50) {
 // } else {
 //   console.log("Sayi tektir");
 // }
+
+//! ******* IF - ELSE IF - ELSE ******* //
+
+// if (sart1) {
+//     sart1 durumu dogru ise bu blok calistirilir.
+// } else if (sart2) {
+//     sart1 durumu dogru degil, sart2 durumu dogru ise bu blok calistirilir.
+//  } else {
+//     sart durumu dogru degilse bu blok calistirilir.
+//  }
+
+//! HESAP MAKINESI
+
+console.log("*******Hesap Makinesi*******");
+
+// 7 + 5 = 12
+// 7- 5 = 2
+// 7 * 5 = 35
+// 7 / 5 = 1
+
+const sayi1 = +prompt("1.sayiyi griniz");
+const islem = prompt(" Bir operatör giriniz(+,-,*,/):");
+const sayi2 = +prompt("2.sayiyi giriniz");
+
+let sonuc;
+
+if (islem == "+") {
+  sonuc = sayi1 + sayi2;
+} else if (islem == "-") {
+  sonuc = sayi1 - sayi2;
+} else if (islem == "*") {
+  sonuc = sayi1 * sayi2;
+} else if (islem == "/") {
+  sonuc = sayi1 / sayi2;
+} else {
+  console.log("Yanlis operatör girdiniz");
+}
+
+console.log("SONUC", sonuc);
+
+if (sonuc != 0) {
+  console.log("Tebrikler bir sonuc buldunuz", sonuc);
+}
+
+//* 1.Kisayol
+if (sonuc) {
+  console.log("Tebrikler bir sonuc buldunuz", sonuc);
+}
+
