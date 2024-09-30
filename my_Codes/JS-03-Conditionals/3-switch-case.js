bb537e27c0ecc50b9162bea086aa2346413d8fa4
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-30 15:09:02 +0200

    25.commit

--- my_Codes/JS-03-Conditionals/3-switch-case.js
+++ my_Codes/JS-03-Conditionals/3-switch-case.js
@@ -5,5 +5,28 @@
 // Burda bir şart durumunun karşılasacağı birden fazla ihtimale göre ortaya çıkabilme ihtimali olan her duruma karşılık bir yapı verilir .
 // Switch de önemli olan şey; bir şartla karşılaştığı zaman onu uygular ve devam eder işte o noktada çıkmasını istiyoruz ve kod blogunu kırıp çıkmazsak (with break) sadece sonuncu değeri yazar.
 
-// let urun = prompt("Bir ürün giriniz");
+// birden fazla satirlik kod olacaksa {} acilmalidir
+let urun = prompt("Bir ürün giriniz");
 let fiyat;
+let urunAdedi = 10;
+let tutar;
+
+switch (urun) {
+  case "kalem": {
+    fiyat = "kalem 5$";
+    tutar = urunAdedi * 5;
+    break;
+  }
+  case "defter": {
+    fiyat = "defter 10$";
+    tutar = urunAdedi * 10;
+    break;
+  }
+  default: {
+    console.log("gecerli bir ürün giriniz");
+    break;
+  }
+}
+
+console.log(fiyat);
+console.log(tutar);
