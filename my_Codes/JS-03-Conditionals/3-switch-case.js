5fafc403ac370ad724d19ec86fb787369dff5058
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-30 16:12:31 +0200

    28.commit

--- my_Codes/JS-03-Conditionals/3-switch-case.js
+++ my_Codes/JS-03-Conditionals/3-switch-case.js
@@ -123,3 +123,57 @@ switch (day) {
     console.log("Saturday");
     break;
 }
+
+//ÖRNEK SORULAR
+
+//1.Bir kullanici girisine göre selamlama yapma:
+//tr:merhaba
+//en:hello
+//fr:bonjour
+
+//2.Bir ögrenci notuna göre harf notunu belirleme:
+//90 üzeri A
+//80 üzeri B
+//70 üzeri C
+//60 üzeri D
+//alti F degeri
+
+//3.Kideme göre alinacak promosyon miktarini gösteren programi switch-case ile olusturalim:
+
+//prof: 1000$
+//senior: 700$
+//junior: 300$
+//default: 100$
+
+let maas = 1000;
+let kidem = prompt("personelin kidemini giriniz");
+let promosyon;
+
+// örn: profesörün promosyonu maas+1000 olacaktır
+
+// 4.Bir alışverişte toplam tutara göre indirim hesaplama:
+// let totalAmount = 150;
+// 200den büyükse indirim 20%
+// 100den büyükse indirim 10%
+// bunun dışında indirim yok  :) "fakir bu kadarcık mı alışveriş yaptın" yazalım
+
+// 5.Bir kullanıcıya ait yaşa göre kategori belirleme:
+// yaş 2den küçükse çocuk
+// 13-19 arası Genç
+// 20-64 arası Yetişkin
+// 65 üzeri Yaşlı  yazdıralım
+
+// 6.switch-case yapısı kullanılarak bir sıcaklık değerine göre hangi kıyafeti giymeniz gerektiğini belirleyen bir JavaScript kodu yazınız. Kod, sıcaklık değerine bağlı olarak şunları yapmalıdır:
+
+// Eğer sıcaklık 0°C'den düşükse, "Kalın mont giyin" yazdırmalıdır.
+// Eğer sıcaklık 0°C ile 10°C arasında ise, "Ceket giyin" yazdırmalıdır.
+// Eğer sıcaklık 10°C ile 20°C arasında ise, "Kazak giyin" yazdırmalıdır.
+// Eğer sıcaklık 20°C ve üzerindeyse, "Tişört giyin" yazdırmalıdır.
+// Herhangi bir geçersiz sıcaklık değeri için "Geçersiz sıcaklık" yazdırmalıdır.
+
+// 7.switch-case yapısını kullanarak bir telefon modeline göre fiyatını belirleyen bir JavaScript kodu yazınız. Kod, telefon modeline göre şunları yapmalıdır:
+
+// Eğer telefon modeli "iPhone 13" ise, "Fiyat: 20,000 TL" yazdırmalıdır.
+// Eğer telefon modeli "Samsung Galaxy S21" ise, "Fiyat: 18,000 TL" yazdırmalıdır.
+// Eğer telefon modeli "OnePlus 9" ise, "Fiyat: 15,000 TL" yazdırmalıdır.
+// Herhangi bir telefon modeli için "Telefon modeli bulunamadı" yazdırmalıdır.
