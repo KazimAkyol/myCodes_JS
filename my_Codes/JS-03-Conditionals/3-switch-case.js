b6059844c19b4addf4c3fdbc07ec932f81c89c5d
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-30 15:56:03 +0200

    27.commit

--- my_Codes/JS-03-Conditionals/3-switch-case.js
+++ my_Codes/JS-03-Conditionals/3-switch-case.js
@@ -33,9 +33,9 @@
 
 //********** hesap makinesi örnegi - switch-case ********* */
 
-const sayi1 = +prompt("1.sayiyi giriniz");
-const islem = prompt("Bir operator giriniz(+,-,*,/):");
-const sayi2 = +prompt("2.sayiyi giriniz");
+// const sayi1 = +prompt("1.sayiyi giriniz");
+// const islem = prompt("Bir operator giriniz(+,-,*,/):");
+// const sayi2 = +prompt("2.sayiyi giriniz");
 
 // let sonuc;
 
@@ -57,33 +57,69 @@ const sayi2 = +prompt("2.sayiyi giriniz");
 // }
 // console.log(sonuc);
 
-switch (islem) {
-  case "+":
-    console.log(sayi1 + sayi2);
-    break;
-  case "-":
-    console.log(sayi1 - sayi2);
-    break;
-  case "*":
-    console.log(sayi1 * sayi2);
-    break;
-  case "/":
-    console.log(sayi1 / sayi2);
-    break;
-  default:
-    console.log("Gecerli bir operatör giriniz");
-}
+// switch (islem) {
+//   case "+":
+//     console.log(sayi1 + sayi2);
+//     break;
+//   case "-":
+//     console.log(sayi1 - sayi2);
+//     break;
+//   case "*":
+//     console.log(sayi1 * sayi2);
+//     break;
+//   case "/":
+//     console.log(sayi1 / sayi2);
+//     break;
+//   default:
+//     console.log("Gecerli bir operatör giriniz");
+// }
 
-let ay = 7;
+// let ay = 7;
 
-switch (ay) {
-    case (ay === 12 || ay === 1 || ay === 2):{
-        console.log("kis");
-        break;
-    }
-    case (ay ===3 || ay === 4 || ay === 5):{
-        console.log("ilkbahar");
-        break;
-    }
-    case 
+// switch (ay) {
+//   case ay === 12 || ay === 1 || ay === 2: {
+//     console.log("kis");
+//     break;
+//   }
+//   case ay === 3 || ay === 4 || ay === 5: {
+//     console.log("ilkbahar");
+//     break;
+//   }
+//   case ay === 6 || ay === 7 || ay === 8: {
+//     console.log("yaz");
+//     break;
+//   }
+//   case ay === 9 || ay === 10 || ay === 11: {
+//     console.log("sonbahar");
+//     break;
+//   }
+//   default:
+//     console.log("Gecersiz bir ay girdiniz");
+// }
+
+const day = new Date().getDay();
+console.log(day);
+
+switch (day) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
 }
