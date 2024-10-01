6288c23cc9426fc145c6c386f4df8980a993b57c
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-01 16:32:07 +0200

    35.commit

--- my_Codes/JS-10-Array Methods/2-iteration.js
+++ my_Codes/JS-10-Array Methods/2-iteration.js
@@ -15,9 +15,46 @@ let toplam = 0;
 
 const terminal = prices1.forEach((a) => console.log((toplam += a))); // her bir elemani bir sonrakiyle topladi ve o indis'e yazdirdi
 
+//* Ödev-1: fiyatlar dizisinde her bir fiyata %10 zam yapalim. NOT: forEach kullanilmali ve orijinal dizi degistirilmeli:
 
+const fiyatlar = [100, 250, 50, 89];
 
+fiyatlar.forEach((fiyat, raise) => {
+  fiyatlar[raise] = fiyat * 1.1;
+});
 
+console.log(fiyatlar);
 
-//* Ödev1: fiyatlar dizisinde her bir fiyata %10 zam yapalim. NOT: forEach kullanilmali ve orijinal dizi degistirilmeli.
-const fiyatlar = [100, 250, 50, 89]; 
+//* Ödev-2: fiyatlar dizisindeki fiyati 90'dan büyük olan degerleri console'a tek tek bastiriniz:
+
+const prices11 = [100, 250, 50, 89];
+
+prices11.forEach((price) => {
+  if (price > 90) {
+    console.log(price);
+  }
+});
+
+//* Ödev-3: fiyatlar dizisindeki fiyati 110'dan kücük olan degerlere %10 artis yapin ve bu degerleri console'a tek tek bastiriniz:
+
+const prices12 = [100, 250, 50, 89];
+
+prices12.map((p, i, array) => {
+  if (p < 110) {
+    array[i] = p * 1.1;
+    console.log(prices12);
+  }
+});
+
+console.log(prices12);
+
+//* Ödev-4: maaslar dizisinde 4000'den düsük olan maaslara %50 zam yapmak istiyoruz ve bunu ayri dizi olarak saklamak istiyoruz:
+
+const maaslar = [3000, 5000, 4000, 6000, 6500];
+
+maaslar.forEach((maas, raise) => {
+  if (maas < 4000) {
+    maaslar[raise] = maas * 1.5;
+    console.log(maaslar);
+  }
+});
