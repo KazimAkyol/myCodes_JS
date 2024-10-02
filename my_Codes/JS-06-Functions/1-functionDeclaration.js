e691060247694ef49e2f2ca6d5ecfad1311f622d
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-02 15:57:52 +0200

    46.commit

---
+++ my_Codes/JS-06-Functions/1-functionDeclaration.js
@@ -0,0 +1,21 @@
+// ? ==========================================================
+// ?                    FONKSİYONLAR
+// ? ==========================================================
+
+// !-----------------------------------------------------------
+// ! 1.YÖNTEM  : FUNCTION DECLARATION
+// !-----------------------------------------------------------
+
+//? Bir fonksiyon belirli bir görevi gerceklestirmek icin tasarlanmis, yeniden kullanilabilir, (tek bir görev icin tek fonksiyon olmasi daha iyidir) test etmesi kolaydir.
+
+//! Fonksiyon Tanimlama Yöntemleri:
+// Function Declaration (Fonksiyon Bildirimi)
+// Function Expression (Fonksiyon Ifadesi)
+// Arrow Function (Ok Fonksiyonlari)
+// Fonksiyon Constructor (Fonksiyon Olusturucusu)
+
+//! * -------- Function-Declaration ----------------------------*/
+// 1-Fonksiyonlar function anahtar kelimesiyle tanimlanir.
+// 2-function kelimesinin ardindan fonksiyonun ismi verilir.
+// 3-Sonra da bir parantez acilip kapatilir.
+//? Fonksiyonun calismasi icin cagirmak gereklidir. Fonksiyon cagirilmadiginda isleme alinmaz. Bu cagirma islemine invoke ya da call denir.
