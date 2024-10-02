9c5d6c13163c0041fa4eb6cf8be65402d48adcd0
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-02 15:33:42 +0200

    45.commit

--- my_Codes/JS-04-Loops/3-doWhile.js
+++ my_Codes/JS-04-Loops/3-doWhile.js
@@ -11,3 +11,42 @@
 let password = "1234.)";
 
 let userPassword;
+
+do {
+  userPassword = prompt("Enter your a Password:");
+
+  if (userPassword !== password) {
+    console.log("Hatali Giris yaptiniz, Yeniden giriniz");
+  }
+} while (userPassword !== password);
+
+//* Kullanicidan sayi istiyoruz. Sayi girilmediyse tekrar girsin:
+
+let sayi;
+
+do {
+  sayi = prompt("Bir sayi giriniz:");
+} while (isNaN(sayi));
+
+//* Sayi Tahmini:
+
+let devam = "";
+do {
+  const sayi = Math.trunc(Math.random() * 50);
+  console.log(sayi);
+  let hak = 5;
+
+  while (hak > 0) {
+    hak--;
+    tahmin = +prompt("Bir sayi tahmin ediniz:");
+    if (tahmin === sayi) {
+      console.log("Buldunuz");
+      break;
+    } else if (tahmin > sayi) {
+      console.log("AZALT");
+    } else {
+      console.log("ARTTIR");
+    }
+  }
+  devam = prompt("Devam etmek ister misin(E):");
+} while (devam === "E");
