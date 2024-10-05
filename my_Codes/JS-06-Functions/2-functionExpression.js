41f86e24103c5c4a0d96a41beffca21f09913f96
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-05 16:22:21 +0200

    68.commit

--- my_Codes/JS-06-Functions/2-functionExpression.js
+++ my_Codes/JS-06-Functions/2-functionExpression.js
@@ -17,4 +17,44 @@ const isEvenOdd = function (sayi) {
   return sayi % 2 == 0 ? "cifttir" : "tektir";
 };
 
-console.log(isEvenOdd(34));
+console.log(isEvenOdd(34)); //cifttir
+
+const result = isEvenOdd(35);
+console.log(result); //tektir
+
+//* Örnek-2:
+
+const buyukBul = function (a, b, c) {
+  let enBuyuk; // function scope
+  if (a > b && a > c) {
+    enBuyuk = a;
+  } else if (b > a && b > c) {
+    enBuyuk = b;
+  } else enBuyuk = c;
+
+  console.log(enBuyuk);
+};
+
+buyukBul(243, 475.78); //475
+
+//* Örnek-3: (arguments keyword'ü ile):
+
+const enBul = function () {
+  let enBuyuk = arguments[0];
+  let enKucuk = arguments[0];
+
+  for (let i = 1; i < arguments.length; i++) {
+    if (arguments[i] > enBuyuk) {
+      enBuyuk = arguments[i];
+    }
+
+    if (arguments[i] < enKucuk) {
+      enKucuk = arguments[i];
+    }
+  }
+  return `en büyük sayi ${enBuyuk}, en kücük sayi ${enKucuk}`;
+};
+
+console.log(enBul(34, 111, 777, 678, 56, 2, 5678, 34, 6));
+
+//* Örnek-4: Bir fonksiyon icinden baska bir fonksiyon cagirilabilir.
