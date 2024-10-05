23858a23bf857b75018672de9f87a899b248ef77
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-05 17:29:34 +0200

    70.commit

--- my_Codes/JS-06-Functions/3-functionArrow.js
+++ my_Codes/JS-06-Functions/3-functionArrow.js
@@ -11,3 +11,35 @@ console.log("****** 3- ARROW FUNCTIONS ******");
 //! Function expression ve arrow function yöntemlerinde önce fonksiyon tanimlanmalidir sinra cagrilmalidir.Aksi takdirde hata alirsiniz.
 
 //* Örnek-1: Girilen sayinin 3'ün kati olup olmadigini kontrol eden fonksiyon:
+
+const kontrol = (sayi) =>
+  sayi % 3 == 0 ? "bu sayi 3'ün katidir" : "bu sayi 3'ün kati degildir";
+console.log(kontrol(34));
+
+const kontrol2 = (sayi) => {
+  return sayi % 3 == 0 ? "bu sayi 3'ün katidir" : "bu sayi 3'ün kati degildir";
+};
+console.log(kontrol2(51));
+
+//* Örnek-2 : Menu fonksiyon
+//! ok(arrow) fonsiyonunda birden fazla ifade varsa, fonksiyonda süslü parantez kullanmaliyiz.
+
+const menu = () => {
+  console.log("=========================");
+  console.log("    JAVASCRIPT DERSI     ");
+  console.log("=========================");
+
+  const a = "=========================";
+  const b = "    JAVASCRIPT DERSI     ";
+  const c = "=========================";
+
+  return `${a} ${b} ${c}`;
+};
+
+console.log(menu());
+
+//* Örnek-3: Silindirin hacmini hesapla (pi*r**2*h):
+
+const hacimHesapla = (r, h) => Math.PI * r * r * h;
+
+console.log(hacimHesapla(3, 5).toFixed(2));
