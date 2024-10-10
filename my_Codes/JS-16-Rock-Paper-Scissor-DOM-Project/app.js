f47bf4d0bcb8e7425569bee60c0a3a406f50fb0e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-10 23:34:26 +0200

    110.commit

--- my_Codes/JS-16-Rock-Paper-Scissor-DOM-Project/app.js
+++ my_Codes/JS-16-Rock-Paper-Scissor-DOM-Project/app.js
@@ -44,5 +44,85 @@ selectionArticle.addEventListener("click", (e) => {
 
   if (e.target.id) {
     userSelectImg.src = `./assets/${e.target.id}.png`;
+    userSelectImg.alt = e.target.id;
+    yourChoiseDiv.appendChild(userSelectImg);
+    createPcSelection();
   }
 });
+
+playAgainButton.addEventListener("click", () => {
+  console.log("Play again butonuna tiklandi");
+  // modalCardSection.classList.toggle("show");
+  // modalCardSection.classList.toggle("remove");
+  modalCardSection.style.display = "none";
+  window.location.reload();
+});
+
+//! Functions
+
+const createPcSelection = () => {
+  pcArr = ["rock", "paper", "scissor"];
+  pcRandom = pcArr[Math.floor(Math.random() * 3)];
+  // console.log("test", Math.floor(Math.random() * 3));
+  pcSelecetImg.src = `./assets/${pcRandom}.png`;
+  pcSelecetImg.alt = pcRandom;
+  pcChoiseDiv.appendChild(pcSelecetImg);
+  calculateResult();
+};
+
+const calculateResult = () => {
+  console.log("user", userSelectImg.alt);
+  console.log("pc", pcSelecetImg.alt);
+
+  //Esitlik Durumu
+  if (userSelectImg.alt == pcRandom) {
+    draw();
+  } else {
+    if (userSelectImg.alt === "rock") {
+      pcRandom === "paper" ? youLost() : youWin();
+    } else if (userSelectImg.alt === "scissor") {
+      pcRandom === "rock" ? youLost() : youWin();
+    } else if (userSelectImg.alt === "paper") {
+      pcRandom === "scissor" ? youLost() : youWin();
+    }
+  }
+  if (pcScoreSpan.textContent === "10" || yourScoreSpan.textContent === "10") {
+    openModal();
+  }
+};
+
+const draw = () => {
+  messagePar.textContent = "It's a draw";
+  scoreCardSection.style.color = YELLOW;
+  messagePar.style.backgroundColor = YELLOW;
+};
+
+const youLost = () => {
+  messagePar.textContent = "You Lost";
+  scoreCardSection.style.color = RED;
+  messagePar.style.backgroundColor = RED;
+  pcScoreSpan.textContent++;
+};
+
+const youWin = () => {
+  messagePar.textContent = "You Win";
+  scoreCardSection.style.color = GREEN;
+  messagePar.style.backgroundColor = GREEN;
+  yourScoreSpan.textContent++;
+  topScoreCheck();
+};
+
+const openModal = () => {
+  console.log("Modal Acildi");
+  modalCardSection.classList.add("show");
+  if (yourScoreSpan.textContent === "10") {
+    finalMEssagePar.textContent = "💃 You Win";
+    document, (querySelector(".modal").style.backgroundColor = GREEN);
+  } else {
+    finalMEssagePar.textContent = "😩 You Lost";
+    document.querySelector(".modal").style.backgroundColor = RED;
+    playAgainButton.style.color = RED;
+  }
+};
+
+
