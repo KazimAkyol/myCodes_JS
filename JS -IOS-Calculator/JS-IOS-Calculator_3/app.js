4d2770a66f56534023be1cca6a7658dfb1fe342f
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-13 16:52:31 +0200

    124.commit

--- JS -IOS-Calculator/JS-IOS-Calculator_3/app.js
+++ JS -IOS-Calculator/JS-IOS-Calculator_3/app.js
@@ -1,3 +1,25 @@
 const hourE1 = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const valueEl = document.querySelector(".value");
+
+const acE1 = document.querySelector(".ac");
+const pmE1 = document.querySelector(".pm");
+const percentE1 = document.querySelector(".percent");
+
+const additionE1 = document.querySelector(".addition");
+const subtractionE1 = document.querySelector(".subtraction");
+const multiplicationE1 = document.querySelector(".multiplication");
+const divisonE1 = document.querySelector(".division");
+const equalE1 = document.querySelector(".equal");
+
+const decimalE1 = document.querySelector(".decimal");
+const number0E1 = document.querySelector(".number-0");
+const number1E1 = document.querySelector(".number-1");
+const number2E1 = document.querySelector(".number-2");
+const number3E1 = document.querySelector(".number-3");
+const number4E1 = document.querySelector(".number-4");
+const number5E1 = document.querySelector(".number-5");
+const number6E1 = document.querySelector(".number-6");
+const number7E1 = document.querySelector(".number-7");
+const number8E1 = document.querySelector(".number-8");
+const number9E1 = document.querySelector(".number-9");
