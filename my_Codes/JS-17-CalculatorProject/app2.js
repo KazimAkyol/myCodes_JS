4f09f56f88c2215b307bc4d932749370b3c3751c
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-14 18:17:34 +0200

    130.commit

--- my_Codes/JS-17-CalculatorProject/app2.js
+++ my_Codes/JS-17-CalculatorProject/app2.js
@@ -36,4 +36,92 @@ const ekranaHazirlik = (number) => {
   //? kullanici herhango bir yerde . girmisken, tekrar nokta girmeye calisirsa giremesin
 
   if (number == "." && altEkranSayi.includes(".")) return;
+
+  //? ekrana 10 basamakli sayi girilebilsin, 11.basamak girilmesin
+
+  if (altEkranSayi.length > 9) return;
+
+  //? kullanici ilk basta 0 girer, ardindan tekrar 0 girerse, girilmesin, tek 0 döndürsün
+
+  if (altEkranSayi === "0" && number === "0") return;
+
+  //? bütün sartlari basari ile gectiyse basilan numaralari arka arkaya ekle
+
+  altEkranSayi += number;
 };
+
+//! burada yapilanlari ekrana bastir
+
+const updateEkran = () => {
+  //? ilk sayilar altekran'da görünsün
+
+  altEkranDiv.textContent = altEkranSayi;
+
+  //? islem girilince
+  // üstekran'da altta yazan rakam + islem gözükmeli
+
+  if (islem) {
+    ustEkranDiv.textContent = ustEkranSayi + " " + islem;
+  } else {
+    ustEkranDiv.textContent = "";
+  }
+};
+
+//* herhangi bir isleme tiklandiginda
+
+operationButtons.forEach((op) => {
+  op.onclick = () => {
+    //? altekran bosken, hicbir sekilde sayi girisi yapilmamissa, operatöre basilmasin. Bos return yapmaya calistigimiz islemi yaptirmaz.
+    //? return fonksiyon icerisinde her yerde kullanilabilir. Kod return satirina eristiginde fonksiyon durur ve deger fonksiyonun cagirildigi yere geri gönderilir. Bir fonksiyon icerisinde birden fazla return fonksiyonu da olabilir ve return deger döndürmek zorunda degildir.
+
+    if (altEkranSayi === "") return;
+
+    //? arka arkaya islem yapilirsa (esittir'e basmadan hesaplama yapilsin)
+    if (altEkranSayi && ustEkranSayi) hesapla();
+
+    islem = op.textContent;
+
+    ustEkranSayi = altEkranSayi;
+
+    altEkranSayi = "";
+
+    updateEkran();
+  };
+});
+
+//* esittir butonuna tiklandiginda
+
+equalButtons.onclick = () => {
+  hesapla();
+
+  updateEkran();
+};
+
+//* hesapla fonksiyonu
+
+const hesapla = () => {
+  let sonuc;
+  switch (islem) {
+    case "+":
+      sonuc = Number(ustEkranSayi) + Number(altEkranSayi);
+      break;
+    case "-":
+      sonuc = ustEkranSayi - altEkranSayi;
+      break;
+    case "x":
+      sonuc = ustEkranSayi * altEkranSayi;
+      break;
+    case "÷":
+      sonuc = ustEkranSayi / altEkranSayi;
+      break;
+    default:
+      break;
+  }
+
+  //! hesaplama yapildiktan sonra ekranda olmasini istediklerimi vs-code'da güncelleyip updateEkran()'i cagirmaliyim
+  altEkranSayi = sonuc;
+  ustEkranSayi = "";
+  islem = "";
+};
+
+
