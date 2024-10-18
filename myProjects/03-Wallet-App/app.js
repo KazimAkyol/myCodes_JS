d9057faa1a540f27c6db49e3498542289aa0115e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-18 17:02:19 +0200

    148.commit

--- myProjects/03-Wallet-App/app.js
+++ myProjects/03-Wallet-App/app.js
@@ -22,7 +22,7 @@ const temizleBtn = document.getElementById("temizle-btn");
 
 let gelirinizTable = document.getElementById("geliriniz");
 const giderinizTable = document.getElementById("gideriniz");
-const kalanTabl = document.getElementById("kalan");
+const kalanTable = document.getElementById("kalan");
 
 //? Variables
 
@@ -31,3 +31,92 @@ let gelirim = Number(localStorage.getItem("gehalt")) || 0;
 let harcamaListesi = JSON.parse(localStorage.getItem("einkaufen")) || [];
 
 //! ekle formu (yesil-kirmizi)
+
+ekleFormu.onsubmit = (e) => {
+  e.preventDefault(); // submit'in default hareketini bekletmek ve diger verilerin girdisini saglamqk icin
+
+  gelirim = gelirim + Number(gelirInput.value);
+
+  gelirinizTable.textContent = gelirim;
+
+  gelirInput.value = "";
+
+  localStorage.setItem("gehalt", gelirim);
+
+  hesaplaveGuncelle();
+};
+
+//! ilk harcama formu doldurma (arka plani sari olan yer)
+
+harcamaFormu.onsubmit = (e) => {
+  e.preventDefault();
+
+  const yeniHarcama = {
+    tarih: tarihInput.value,
+    miktar: miktarInput.value,
+    alan: harcamaAlaniInput.value,
+    id: new Date().getTime(),
+  };
+
+  harcamaListesi.push(yeniHarcama); // veriler girildikten sonra dizinin sonuna ekleme yapmak icin push() kullanildi
+
+  // harcamaListesi.push({
+  //   tarih: tarihInput.value,
+  //   miktar: miktarInput.value,
+  //   alan: harcamaAlaniInput.value,
+  //   id: new Date().getTime(),
+  // });
+
+  //* localStorage'a kaydet
+
+  localStorage.setItem("einkaufen", JSON.stringify(harcamaListesi));
+
+  //* ekrana bastir
+
+  harcamayiDomaYaz(yeniHarcama);
+
+  //* inputlari temizle
+
+  harcamaFormu.reset();
+
+  hesaplaveGuncelle();
+};
+
+//! harcamayi doma yazma metodu
+
+const harcamayiDomaYaz = (yeniHarcama) => {
+  const { id, miktar, tarih, alan } = yeniHarcama; // destruction
+
+  harcamaBody.innerHTML += ` 
+  <tr>
+      <td class="bg-warning bir">${tarih}</td>
+      <td class="bg-warning">${alan}</td>
+      <td class="bg-warning">${miktar}</td>
+
+      <td class="bg-warning">
+      <i id=${id} class="fa-solid fa-trash-can text-danger"></i>
+      </td>
+    </tr>`;
+
+  //! silme
+
+  document.querySelectorAll(".fa-trash-can").forEach((sil) => {
+    sil.onclick = () => {
+      //* ekrandan sil
+
+      sil.parentElement.parentElement.remove();
+
+      // sil.closest("tr").remove(); // parent'ini silmenin 2.yolu
+
+      //* diziden sil
+
+      harcamaListesi = harcamaListesi.filter((a) => a.id != sil.id);
+
+      //* localStorage'de de sil
+
+      localStorage.setItem("einkaufen", JSON.stringify(harcamaListesi));
+    };
+  });
+};
+
+//! hesapla ve güncelle
