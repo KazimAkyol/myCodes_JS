f5f8a7fb8d7b8dd354e28af89a7c23ca8ef156eb
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-20 22:52:49 +0200

    155.commit

--- shop website/main.js
+++ shop website/main.js
@@ -7,8 +7,10 @@ eventListeners();
 function eventListeners() {
   window.addEventListener("load", function () {
     let carts = GetCartsFromStorage();
-    this.document.getElementById("cart-count").innerText[i].innerText =
-      carts.length;
+    //this.document.getElementById("cart-count").innerText = carts.length;
+
+    let page = this.window.location.pathname;
+    if (page == "./detail.html") FillDetailTable();
 
     for (let i = 0; i < cart_btn.length; i++)
       cart_btn[i].addEventListener("click", AddCart);
@@ -40,7 +42,7 @@ function GetCartsFromStorage() {
   let carts;
 
   if (localStorage.getItem("carts") === null) carts = [];
-  else carts = JSON(localStorage.getItem("carts"));
+  //else carts = JSON(localStorage.getItem("carts"));
 
   return carts;
 }
@@ -51,15 +53,20 @@ function AddCartsToStorage(data) {
   let carts = GetCartsFromStorage();
   carts.push(data);
   localStorage.setItem("carts", JSON.stringify(carts));
+
+  document.getElementById("cart-count").innerHTML = carts.length;
 }
 
 //! veri silme
 
-function DeleteCartsFromStorage(id) {
+function DeleteCartFromStorage(id) {
   let carts = GetCartsFromStorage();
 
   carts.splice(id, 1);
   localStorage.setItem("carts", JSON.stringify(carts));
+  document.getElementById("cart-count").innerText = carts.length;
+
+  FillDetailTable();
 }
 
 function ShowAlert() {
@@ -77,20 +84,30 @@ function GetShoppingCartPage() {
   window.location.href = "./index.html";
 }
 
-function FillDetailTable(){
-    let html=""
-    let carts = GetCartsFromStorage()
+function FillDetailTable() {
+  let html = "";
+  let carts = GetCartFromStorage();
 
-    for(let i=0; i<carts.length; i++){
-        html+="<tr>"+
-        "<td>"+carts[i].product_name+"</td>"+
-        "<td>"+carts[i].product_price+"</td>"+
-        "<td>"+carts[i].product_count+"</td>"+
-        "<td>"+carts[i].total_Price+"</td>"+
-        "<td><i class="fas fa-trash delete" onclick="DeleteCartFromStorage("+i+");"></i></td>"+
-        "</tr>";
+  for (let i = 0; i < carts.length; i++) {
+    html +=
+      "<tr>" +
+      "<td>" +
+      carts[i].product_name +
+      "</td>" +
+      "<td>" +
+      carts[i].product_price +
+      "</td>" +
+      "<td>" +
+      carts[i].product_count +
+      "</td>" +
+      "<td>" +
+      carts[i].total_Price +
+      "</td>" +
+      '<td><i class="fas fa-trash delete" onclick="DeleteCartFromStorage(' +
+      i +
+      ');"></i></td>' +
+      "</tr>";
 
-        document.getElementById("cart-detail").innerHTML=html;
-        document.getElementById("cart-count").innerText= carts.length-1;
-    }
+    document.getElementById("cart-detail").innerHTML = html;
+  }
 }
