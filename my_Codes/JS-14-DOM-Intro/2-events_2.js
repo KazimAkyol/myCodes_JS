fc5e51343d9301cf0fe3b6df9954ada1c70009d7
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-27 17:18:10 +0100

    162.commit

--- my_Codes/JS-14-DOM-Intro/2-events_2.js
+++ my_Codes/JS-14-DOM-Intro/2-events_2.js
@@ -10,3 +10,27 @@ merhaba.onmouseover = () => {
   badi.style.background = "pink";
   merhaba.style.color = "red";
 };
+
+merhaba.onmouseout = () => {
+  badi.style.background = "orange";
+};
+
+//* example-2: onclick, ondblclick (ondoubleclick)
+
+const bir = document.querySelector(".bir"); // sari JS resim
+const iki = document.querySelector(".iki"); // siyah JS resim
+
+bir.onclick = () => {
+  bir.src = "./img/logo2.png";
+  iki.src = "./img/js_logo.png";
+};
+
+iki.onclick = () => {
+  bir.src = "./img/js_logo.png";
+  iki.src = "./img/logo2.png";
+};
+
+bir.ondblclick = () => {
+  bir.src = "./img/bir.jpeg";
+  iki.src = "./img/iki.webp";
+};
