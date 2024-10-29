5b646e25160db39433ac2b6c2eb42b14cdc22e32
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-29 17:12:22 +0100

    173.commit

--- my_Codes/JS-Async/random_user.js
+++ my_Codes/JS-Async/random_user.js
@@ -1,18 +1,19 @@
 fetch("https://randomuser.me/api")
   .then((res) => res.json())
-  .then((data) => ekranaBastir(data.person));
+  .then((data) => ekranaBastir(data.results[0]));
 
-//! ekrana bastirma
-
+// //! ekrana bastirma
 const ekranaBastir = (z) => {
   const personDiv = document.querySelector(".person");
+  console.log(z);
 
-  z.forEach((k) => {
-    personDiv.innerHTML += `
-        <div>
-        <p>${k.seed}</p>
-        <img width="250px" src=${k.medium} />
-        </div>
-        `;
-  });
+  personDiv.innerHTML += `         
+    <div>         
+    <p>${z.gender}</p>         
+    <img width="250px" src=${z.picture.large} />
+    <p>${z.name.title} ${z.name.first} ${z.name.last}</p>         
+    <p>${z.email}</p>         
+    <p>${z.phone} </p>         
+    </div>         
+    `;
 };
