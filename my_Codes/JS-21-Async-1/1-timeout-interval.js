99da001861e29287c45cf1001c13e4e49b5ecca3
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-30 22:42:50 +0100

    177.commit

--- my_Codes/JS-21-Async-1/1-timeout-interval.js
+++ my_Codes/JS-21-Async-1/1-timeout-interval.js
@@ -10,3 +10,24 @@
 //? Özellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri, Dosya Okuma/Yazma islemleri gibi zaman tüketen kodlarda Async Programlama kullanilmasi cok önemlidir.
 
 //* Asenkron (setTimeout) 1 seferlik
+
+setTimeout(() => {
+  console.log("günaydin nasilsiniz");
+}, 2000); // 2000 milisaniye yani 2 saniye demek. Sayfayi yeniledikten 2 saniye sonra console'a yazdiracak.
+
+console.log("selamun hello");
+
+setTimeout(() => {
+  console.log("aleyküm selam");
+}, 4000); // 4000 milisaniye yani 4 saniye demek. Sayfayi yeniledikten 4 saniye sonra console'a yazdiracak.
+
+//! yukarıdaki kodlar şu sırayla çalışır selamun hello - günaydın nasılsınız - aleyküm selam
+
+//*Asenkron(setInterval, clearInterval) belli aralıklarla durdurulana kadar devam eder. mesela 1 dakikada bir döviz kurunu güncellemek için kullanılabilir.
+
+//?setInterval non-blocking (kod burada takılı kalmaz alttaki kodlarda çalışır)
+
+let sayac = 0;
+setInterval(() => {
+  console.log(++sayac);
+}, 1000);
