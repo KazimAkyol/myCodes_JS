582b4c0661a38c6552d7fe06688152eac4590b43
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-02 12:36:15 +0100

    182.commit

--- my_Codes/JS-21-Async-1/3-chain-fetchApi.js
+++ my_Codes/JS-21-Async-1/3-chain-fetchApi.js
@@ -24,18 +24,59 @@
 
 // console.log("merhaba");
 
+//! butona basilmadan önce verilerin gelmesi icin yazilan kod:
+
+// fetch("https://api.github.com/users")
+//   .then((res) => res.json())
+//   .then((veri) => ekranaBastir(veri));
+
+// const ekranaBastir = (veri) => {
+//   veri.forEach((person) => {
+//     document.querySelector(".users").innerHTML += `
+
+//         <h1> ${person.login} </h1>
+//         <img width = "50%" src = ${person.avatar_url}/>
+//         <h3> ${person.node_id}</h3>`;
+//   });
+// };
+
 //! butona basildiktan sonra verilerin gelmesi icin yazilan kod:
 
-fetch("https://api.github.com/users")
-  .then((res) => res.json())
-  .then((veri) => ekranaBastir(veri));
+document.querySelector(".btn").onclick = () => {
+  fetch("https://api.github.com/users")
+    .then((res) => res.json())
+    .then((veri) => ekranaBastir(veri));
+};
 
 const ekranaBastir = (veri) => {
   veri.forEach((person) => {
     document.querySelector(".users").innerHTML += `
-        
-        <h1> ${person.login} </h1>
-        <img width = "50%" src = ${person.avatar_url}/>
-        <h3> ${person.node_id}</h3>`;
+          
+          <h1> ${person.login} </h1>
+          <img width = "50%" src = ${person.avatar_url}/>
+          <h3> ${person.node_id}</h3>`;
   });
 };
+
+//! butona basildiktan sonra hatayi bulmak icin yazilan kod:
+
+document.querySelector(".btn").onclick = () => {
+  fetch("https://api.github.com/users")
+    .then((res) => {
+      if (res.ok != true) {
+        throw new Error("url'de hata var");
+      }
+
+      return res.json();
+    })
+    .then((veri) => ekranaBastir(veri));
+};
+
+const ekranaBastir = (veri) => {
+    veri.forEach((person) => {
+        document.querySelector(".users").innerHTML += `
+        <h1> ${person.login} </h1>
+        <img width
+        `
+    })
+}
