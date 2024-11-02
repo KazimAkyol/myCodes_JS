69be737cada9437bba592cbc2fda2b3705edbf67
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-02 21:11:15 +0100

    185.commit

--- my_Codes/JS-21-Async-1/4-async-await-fetch.js
+++ my_Codes/JS-21-Async-1/4-async-await-fetch.js
@@ -26,12 +26,67 @@
 const defaultImage =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/220px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg";
 
-const getirData = async () => {
-  await fetch("https://api.tvmaze.com/search/shows?q=girls"); // await komutu yazilan kodun bir süre beklemesini saglar ve süslü parantezin disina yazilan console.log("merhaba"); 'dan sonra console'a yazilir.
+//! async-await-fetch yazimi:
 
-  // console.log("asdfghjkl");
+// const getirData = async () => {
+//   const res = await fetch("https://api.tvmaze.com/search/shows?q=girls"); // async-await komutu yazilan kodun bir süre beklemesini saglar ve süslü parantezin disina yazilan console.log("merhaba"); 'dan sonra console'a yazilir.
+
+//   const veri = await res.json();
+
+//   ekranaBastir(veri);
+
+//   // console.log("asdfghjkl");
+
+//   // console.log(res);
+// };
+
+// getirData();
+
+// // console.log("merhaba");
+
+// const ekranaBastir = (data) => {
+//   // console.log(data);
+
+//   data.forEach((a) => {
+//     document.querySelector(".users").innerHTML += `
+//     <h1> ${a.show.name} </h1>
+//     <img src = "${a.show.image?.medium || defaultImage}"/>
+//     `; // eger image cikmazsa defaultImage olarak eklenen linki yani Mona Lisa'yi kullanir.
+//   });
+// };
+
+//! async-await-fetch hata yazimi: ekrana 404 ERROR yazdirma
+
+const getirData = async () => {
+  try {
+    const res = await fetch("https://api.tvmaze.com/search/shows?"); // API linkini eksik yazinca 404 ERROR yazisini elde etmek icin yazilan kod
+
+    if (res.ok != true) {
+      throw new Error(`url'de hata var ${res.status} `);
+    }
+
+    const veri = await res.json();
+    ekranaBastir(veri);
+  } catch (error) {
+    console.log(error);
+    console.log("try-catch sayesinde koda devam edilebilir");
+
+    document.querySelector(".users").innerHTML = `
+        <img src ="./img/404.png"/>
+        <h1>${error}</h1>`;
+  }
 };
 
 getirData();
 
-// console.log("merhaba");
+const ekranaBastir = (data) => {
+  data.forEach((a) => {
+    console.log(a);
+
+    document.querySelector(".users").innerHTML += `
+        
+        <h1> ${a.show.name} </h1>
+        
+        <img src = "${a.show.image?.medium || defaultImage}"/> `;
+  });
+};
