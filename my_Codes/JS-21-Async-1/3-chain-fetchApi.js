cc9469da146981a7b019bc291f939db2b1fc438f
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-02 10:29:47 +0100

    181.commit

--- my_Codes/JS-21-Async-1/3-chain-fetchApi.js
+++ my_Codes/JS-21-Async-1/3-chain-fetchApi.js
@@ -10,7 +10,7 @@
 
 //?! Bu baglamda syntatic sugar benzetmesi yapilabilir.
 
-//!throw ile hata fırlatırsak kodumuz o satırda durur. Kodumuzu durdurmak istemezsek diğer sayfadaki kodları kullanırız.
+//! throw ile hata fırlatırsak kodumuz o satırda durur. Kodumuzu durdurmak istemezsek diğer sayfadaki kodları kullanırız.
 
 // Informational responses (100 – 199)
 // Successful responses (200 – 299)
@@ -18,10 +18,24 @@
 // Client error responses (400 – 499)
 // Server error responses (500 – 599)
 
+// fetch("https://api.github.com/users")
+//   .then((res) => res.json())
+//   .then((veri) => console.log(veri));
+
+// console.log("merhaba");
+
+//! butona basildiktan sonra verilerin gelmesi icin yazilan kod:
+
 fetch("https://api.github.com/users")
   .then((res) => res.json())
-  .then((veri) => console.log(veri));
+  .then((veri) => ekranaBastir(veri));
 
-console.log("merhaba");
-
-//! bitona basildiktan sonra verilerin gelmesi icin yazilan kod: 
+const ekranaBastir = (veri) => {
+  veri.forEach((person) => {
+    document.querySelector(".users").innerHTML += `
+        
+        <h1> ${person.login} </h1>
+        <img width = "50%" src = ${person.avatar_url}/>
+        <h3> ${person.node_id}</h3>`;
+  });
+};
